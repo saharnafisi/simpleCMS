0a742148a2ecafba1770170bdd941aca96c7b7f1
Sahar Nafisi <sahar.nafisi95@gmail.com>
2017-08-20 14:31:29 +0430

    bugs fixed

--- main.py
+++ main.py
@@ -9,74 +9,81 @@ class MainHandler(tornado.web.RequestHandler):
         if not self.get_cookie("user"):
             self.redirect("/login")
         else:
-            query="SELECT title,id FROM 'article'"
-            cursor=self.application.db.cursor()
+            query = "SELECT title,id FROM 'article'"
+            cursor = self.application.db.cursor()
             cursor.execute(query)
             self.application.db.commit()
-            articles=cursor.execute(query)
-            self.render("mainPage.html",articles=articles)
+            articles = cursor.execute(query)
+            self.render("mainPage.html", articles=articles)
+
 
 class Login(tornado.web.RequestHandler):
     def get(self):
-        self.render("login.html",message=None)
+        self.render("login.html", message=None)
+
     def post(self):
-        username=self.get_argument("userName")
-        password=self.get_argument("password")
-        query="SELECT * FROM 'user' WHERE userName=? AND password=?"
-        cursor=self.application.db.cursor()
-        cursor.execute(query,[username,password])
-        result=cursor.fetchone()
+        username = self.get_argument("userName")
+        password = self.get_argument("password")
+        query = "SELECT * FROM 'user' WHERE userName=? AND password=?"
+        cursor = self.application.db.cursor()
+        cursor.execute(query, [username, password])
+        result = cursor.fetchone()
         if not result:
-            self.render("login.html",message=True)
+            self.render("login.html", message=True)
         else:
+            self.set_cookie("user","sahar")
             self.redirect("/")
-            self.set_cookie("user",username)
+
 class AddArticle(tornado.web.RequestHandler):
     def get(self):
         self.render("addArticle.html")
+
     def post(self):
-        article_id=self.get_argument("id")
-        title=self.get_argument("title")
-        content=self.get_argument("content")
-        query="insert into 'article' values(?,?,?)"
-        cursor=self.application.db.cursor()
-        cursor.execute(query,[article_id,title,content])
+        article_id = self.get_argument("id")
+        title = self.get_argument("title")
+        content = self.get_argument("content")
+        query = "insert into 'article' values(?,?,?)"
+        cursor = self.application.db.cursor()
+        cursor.execute(query, [article_id, title, content])
         self.application.db.commit()
         self.redirect("addArticle")
 
+
 class ShowArticle(tornado.web.RequestHandler):
-    def get(self,article_id):
-        query="SELECT * FROM 'article' WHERE id=?"
-        cursor=self.application.db.cursor()
-        cursor.execute(query,[article_id])
+    def get(self, article_id):
+        query = "SELECT * FROM 'article' WHERE id=?"
+        cursor = self.application.db.cursor()
+        cursor.execute(query, [article_id])
         self.application.db.commit()
-        article=cursor.fetchone()
-        self.render("showArticle.html",article=article)
+        article = cursor.fetchone()
+        self.render("showArticle.html", article=article)
+
 
 class DeleteArticle(tornado.web.RequestHandler):
-    def get(self,article_id):
-        query="DELETE FROM 'article' WHERE id=?"
-        cursor=self.application.db.cursor()
-        cursor.execute(query,[article_id])
+    def get(self, article_id):
+        query = "DELETE FROM 'article' WHERE id=?"
+        cursor = self.application.db.cursor()
+        cursor.execute(query, [article_id])
         self.application.db.commit()
         self.redirect("/")
 
-class UpdateArticle(tornado.web.RequestHandler):
-    def get(self,article_id):
-        query="SELECT * FROM 'article' WHERE id=?"
-        cursor=self.application.db.cursor()
-        cursor.execute(query,[article_id])
-        self.application.db.commit()
-        article=cursor.fetchone()
-        self.render("editArticle.html",article=article)
 
-    def post(self,article_id):
-        #article_id=self.get_argument("id")
-        title=self.get_argument("title")
-        content=self.get_argument("content")
-        query="UPDATE 'article' SET title=?, text=? WHERE id=?"
-        cursor=self.application.db.cursor()
-        cursor.execute(query,[title,content,article_id])
+class UpdateArticle(tornado.web.RequestHandler):
+    def get(self, article_id):
+        query = "SELECT * FROM 'article' WHERE id=?"
+        cursor = self.application.db.cursor()
+        cursor.execute(query, [article_id])
+        self.application.db.commit()
+        article = cursor.fetchone()
+        self.render("editArticle.html", article=article)
+
+    def post(self, article_id):
+        # article_id=self.get_argument("id")
+        title = self.get_argument("title")
+        content = self.get_argument("content")
+        query = "UPDATE 'article' SET title=?, text=? WHERE id=?"
+        cursor = self.application.db.cursor()
+        cursor.execute(query, [title, content, article_id])
         self.application.db.commit()
         self.redirect("/")
 
@@ -89,11 +96,11 @@ if __name__ == "__main__":
 
     app = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/login",Login),
-        (r"/addArticle",AddArticle),
-        (r"/articles/([a-zA-Z0-9]+)",ShowArticle),
-        (r"/deleteArticle/([a-zA-Z0-9]+)",DeleteArticle),
-        (r"/editArticle/([a-zA-Z0-9]+)",UpdateArticle)
+        (r"/login", Login),
+        (r"/addArticle", AddArticle),
+        (r"/articles/([a-zA-Z0-9]+)", ShowArticle),
+        (r"/deleteArticle/([a-zA-Z0-9]+)", DeleteArticle),
+        (r"/editArticle/([a-zA-Z0-9]+)", UpdateArticle)
     ], **settings)
 
     app.db = sqlite3.connect("site.db")
